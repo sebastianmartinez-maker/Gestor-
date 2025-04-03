94a620d96c43a2856141d493cbbaba3502772b16
sebastianmartinez-maker <eduardomartinez@manitasalaobraeducate.edu.gt>
2025-04-02 18:18:54 -0600

    primer envio

---
+++ main.py
@@ -0,0 +1,11 @@
+import random as r
+import string as s
+
+elements = s.ascii_letters + s.ascii_uppercase + s.digits + s.punctuation
+psw = ""
+lenght = int(input("Cuántos caracteres deseas"))
+
+for i in range(lenght):
+    psw += r.choice(elements)
+
+print(f"Tu contraseña ha sido generada:{psw}")
